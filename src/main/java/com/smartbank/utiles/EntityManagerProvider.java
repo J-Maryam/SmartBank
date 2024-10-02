93bf3d8231bcc68229f1c8e1b55899d4dfbec4ae
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-02 15:17:00 +0100

    creation de EntityManagerFactory

---
+++ src/main/java/com/smartbank/utiles/EntityManagerProvider.java
@@ -0,0 +1,21 @@
+package com.smartbank.utiles;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerProvider {
+    private static EntityManagerFactory emf;
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory("default");
+        }
+        return emf;
+    }
+
+    public static void closeEntityManagerFactory() {
+        if(emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+}
