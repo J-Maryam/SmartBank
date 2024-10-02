8715fe53828a0fe11a47852faa25857117ddcf44
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-02 14:44:01 +0100

    creation de l'entite Request

---
+++ src/main/java/com/smartbank/models/Request.java
@@ -0,0 +1,150 @@
+package com.smartbank.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Request implements Serializable {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cin;
+    private LocalDate birthDate;
+    private LocalDate startEmployementDate;
+    private Double monthlyIncome;
+    private boolean hasActivateCredits;
+    private String email;
+    private String phoneNumber;
+    private String type;
+    private String position;
+    private String amount;
+    private int durationsInMonths;
+    private BigDecimal monthlyPayment;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getStartEmployementDate() {
+        return startEmployementDate;
+    }
+
+    public void setStartEmployementDate(LocalDate startEmployementDate) {
+        this.startEmployementDate = startEmployementDate;
+    }
+
+    public Double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(Double monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public boolean isHasActivateCredits() {
+        return hasActivateCredits;
+    }
+
+    public void setHasActivateCredits(boolean hasActivateCredits) {
+        this.hasActivateCredits = hasActivateCredits;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public int getDurationsInMonths() {
+        return durationsInMonths;
+    }
+
+    public void setDurationsInMonths(int durationsInMonths) {
+        this.durationsInMonths = durationsInMonths;
+    }
+
+    public BigDecimal getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public void setMonthlyPayment(BigDecimal monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+}
