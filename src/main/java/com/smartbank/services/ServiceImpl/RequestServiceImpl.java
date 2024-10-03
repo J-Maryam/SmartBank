186e18e981256235677d2080184e7e2948331960
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-03 11:31:43 +0100

    ajout de la methode save dans service

--- src/main/java/com/smartbank/services/ServiceImpl/RequestServiceImpl.java
+++ src/main/java/com/smartbank/services/ServiceImpl/RequestServiceImpl.java
@@ -1,14 +1,21 @@
 package com.smartbank.services.ServiceImpl;
 
 import com.smartbank.models.Request;
+import com.smartbank.repositories.RequestRepository;
 import com.smartbank.services.RequestService;
 
 import java.util.List;
 
 public class RequestServiceImpl implements RequestService {
+    private RequestRepository requestRepository;
+
+    RequestServiceImpl(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
     @Override
     public void save(Request request) {
-
+        requestRepository.save(request);
     }
 
     @Override
