3fdcdbee5c83154480a4b8e69de63379cd9bb7f9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-06 14:18:36 +0100

    ajout des infos de 2eme form

--- src/main/java/com/smartbank/servlets/CoordinationsServlet.java
+++ src/main/java/com/smartbank/servlets/CoordinationsServlet.java
@@ -1,4 +1,23 @@
 package com.smartbank.servlets;
 
-public class CoordinationServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/coordonnees")
+public class CoordinationsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+
+        HttpSession session = request.getSession();
+        session.setAttribute("email", email);
+        session.setAttribute("phone", phone);
+
+        response.sendRedirect("step3.jsp");
+    }
 }
