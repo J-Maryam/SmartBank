33047587cef551e334001384ede4435634487b66
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-07 15:07:50 +0100

    ajout de la classe RequestStatus (qui est une table generer entre request & status)

--- src/main/java/com/smartbank/models/RequestStatus.java
+++ src/main/java/com/smartbank/models/RequestStatus.java
@@ -1,4 +1,60 @@
 package com.smartbank.models;
 
-public class RequestStatus {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class RequestStatus implements Serializable {
+
+    @ManyToMany
+    private Request request;
+
+    @ManyToMany
+    private Status status;
+
+    private LocalDate StatusDate;
+
+    private String Justification;
+
+    public RequestStatus() {}
+
+    public RequestStatus(Request request, Status status, LocalDate StatusDate, String Justification) {
+        this.request = request;
+        this.status = status;
+        this.StatusDate = StatusDate;
+        this.Justification = Justification;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getStatusDate() {
+        return StatusDate;
+    }
+
+    public void setStatusDate(LocalDate statusDate) {
+        StatusDate = statusDate;
+    }
+
+    public String getJustification() {
+        return Justification;
+    }
+
+    public void setJustification(String justification) {
+        Justification = justification;
+    }
 }
